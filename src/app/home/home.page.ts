02436bca10475bc7913a17c5610a4b7386fce098
lucasfernande <lucas.fernandes6192@outlook.com>
2020-12-04 11:06:59 -0300

    projeto finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public resultado: String = "resultado";
+
+  calcular() {
+
+    if(this.precoAlcool && this.precoGasolina) {
+        
+      var pAlcool = parseFloat(this.precoAlcool)
+      var pGasolina = parseFloat(this.precoGasolina)
+
+      /* 
+        (pAlcool/pGasolina)
+        se o resultado for >= 0.7 = usar gasolina
+        se não = usar alcool
+      */
+
+      var res = pAlcool/pGasolina;
+
+      if(res >= 0.7) {
+        this.resultado = "Melhor utilizar gasolina"
+      }
+      else {
+        this.resultado = "Melhor utilizar álcool"
+      }
+    }
+    else {
+        this.resultado = "Preencha os campos"
+    }
+
+  }
 
 }
